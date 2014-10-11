b6031075953278fbe8fe9d3c7d169761b5b1ec50
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-10-10 20:47:08 -0600

    made more commits about array changes

--- Wk1/goal1_assign_duel1/js/main.js
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -30,7 +30,7 @@ Assignment: Goal1: Assignment: Duel1
 
     function fight(){                                                                                   // This function is for the fight itself
 //      console.log('in the fight function');                                                           // This console.log was to help me understand the order in which code is executed
-        alert(competitorOne[0]+":"+competitorOne[2]+" *START* "+competitorTwo[0]+":"+competitorTwo[2]); // This is an alert to start the game. Changed for array
+        alert(competitorOne[0]+":"+competitorOne[2]+"  *START*  "+competitorTwo[0]+":"+competitorTwo[2]); // This is an alert to start the game. Changed for array
 
         for (var i = 0; i < 10; i++){                                                                   // This is a for loop sets up 10 rounds of fighting
             //console.log(i);                                                                           // This console.log helps me determine if my for loop is producing 10 rounds
@@ -55,7 +55,7 @@ Assignment: Goal1: Assignment: Duel1
             console.log(results);                                                                        // This logs the result or information returned by the winnerCheck function
             if (results==="no winner"){                                                                  // This if statement helps determine if there is a winner or if the rounds need to to continue
                 round++;                                                                                 // This adds one to the round
-                alert(competitorOne[0]+":"+competitorOne[2]+"  *ROUND "+round+" OVER"+"*  "+competitorTwo[0]+":"+competitorTwo[2]); //This alert post after every round that does not produce a winner//
+                alert(competitorOne[0]+":"+competitorOne[2]+"  *ROUND "+round+" OVER"+"*  "+competitorTwo[0]+":"+competitorTwo[2]); //This alert post after every round that does not produce a winner// Changed for array
 
             } else{                                                                                     // Else will execute code if there is a winner or if everyone dies.
                 alert(results);                                                                         // This gives the user the results
@@ -68,13 +68,13 @@ Assignment: Goal1: Assignment: Duel1
     function winnerCheck(){                                     // This function will measure the players health and determine if there is a winner
         console.log("in winnerCheck function");                 // This lets me know that my winnerCheck function is running
         var result="no winner";                                 // This variable is for the result. it is set to 'no winner'
-        if (competitorOne[2]<1 && competitorTwo[2]<1)             // This if statement will give the proper result after determining the winner
+        if (competitorOne[2]<1 && competitorTwo[2]<1)           // This if statement will give the proper result after determining the winner ..Changed for array
         {                                                       // The first condition is to determine if the results of both players dropped below 1 health points during the same round.
             result = "You Both Die";                            // This result is if both players health points drop below 1 during the same round
-        } else if(competitorOne[2]<1){                           // This else if condition determines if player one health drops below one first.
-            result =competitorTwo[0]+" WINS!!!";                   // This is the result if player ones health drops below 1 first
-        } else if (competitorTwo[2]<1){                          // This else if condition determines if player twos health drops below one first
-            result = competitorOne[0]+" WINS!!!";                  // This result occurs with player two's health drops below 1 first
+        } else if(competitorOne[2]<1){                          // This else if condition determines if player one health drops below one first...Changed for array
+            result =competitorTwo[0]+" WINS!!!";                // This is the result if player ones health drops below 1 first...Changed for array
+        } else if (competitorTwo[2]<1){                         // This else if condition determines if player twos health drops below one first...Changed for array
+            result = competitorOne[0]+" WINS!!!";               // This result occurs with player two's health drops below 1 first...Changed for array
         }                                                       // End of else if.
         return result;                                          // This returns the information computed by the winnerCheck function which determines the winner.
     }                                                           // End of winnerCheck function.
