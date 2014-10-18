89f1c3f77ddeb64e40bb527dbf679139d118200c
csaunders1976 <chelsiesaunders@chelsies-mbp.pk5001z>
2014-10-17 21:20:46 -0600

    finished comments

--- Wk3/goal2_assign_duel3/js/main.js
+++ Wk3/goal2_assign_duel3/js/main.js
@@ -32,7 +32,7 @@ var competitors =[                          //Created objects for players
 ];                                          // end of object
 
     //initiate round
-    var round=0;                                    // This variable is for the round that will be incremented for the for loop below
+    var round=1;                                    // This variable is for the round that will be incremented for the for loop below
 
     round_txt.innerHTML = "Click \'Eat Cookies\' button to start!";                 //text for initial instructions
     ernie_txt.innerHTML = competitors[0].name + ": " + competitors[0].health;       //prints Ernies info
@@ -42,58 +42,45 @@ function onClick() {                                                            
     eatCookies()
 }
     function eatCookies(){                                                          // This function is for the cookie consumption itself
- 
-        ernie_txt.innerHTML = competitors[0].name + competitors[0].health;
-        ernie_txt.innerHTML = competitors[1].name + competitors[1].health;
 
+        ernie_txt.innerHTML = competitors[0].name + competitors[0].health;          // prints Ernie's name and health
+        ernie_txt.innerHTML = competitors[1].name + competitors[1].health;          // prints Ernie's name and health
 
-        //for (var i = 0; i < 10; i++){                                                                   // This is a for loop sets up 10 rounds of fighting
-            //console.log(i);                                                                           // This console.log helps me determine if my for loop is producing 10 rounds
-            //random formula is - Math.floor(Math.random() * (max - min) + min);                        // This will create a random number so that the results of the fight are unpredictable
-            //var minDamage1 = competitorOne[1] * .5;                                                     // This variable declares the value for minimum damage for player one...I had change to accommodate the new array
-            //var minDamage2 = competitorTwo[1] * .5;                                                     // This variable declares the value for minimum damage for player two..I had change to accommodate the new array
-            var f1 = Math.floor(Math.random() * competitors[0].damage + competitors[0].damage * .5);                // This formula creates a random number player one's damage
-            var f2 = Math.floor(Math.random() * competitors[1].damage + competitors[1].damage * .5);                // This formula creates a random number player two's damage
-       //   console.log(f1);                                                                            // This console.log is to see if my formula worked for f1
-       //   console.log(f2);                                                                            // This console.log is to see if my formula worked for f2
+            var f1 = Math.floor(Math.random() * competitors[0].damage + competitors[0].damage * .5);                // This formula creates a random number for Ernies damage
+            var f2 = Math.floor(Math.random() * competitors[1].damage + competitors[1].damage * .5);                // This formula creates a random number for Berts damage
+       //   console.log(f1);                                                        // This console.log is to see if my formula worked for f1
+       //   console.log(f2);                                                        // This console.log is to see if my formula worked for f2
             //inflict damage
-            competitors[0].health-=f1;                                                                        // This subtracts the random number of damage points to player one's health ...Changed for array
-            competitors[1].health-=f2;                                                                        // This subtracts the random number of damage points to player two's health ...Changed for array
+            competitors[0].health-=f1;                                              // This subtracts the random number of damage points to Ernie's health
+            competitors[1].health-=f2;                                              // This subtracts the random number of damage points to Bert's health
 
-            //console.log(competitorTwo[1]);                                                             // This tells me how much health player one has
-            //console.log(competitorOne[1]);                                                             // This tells me how much health player two has
-
-            //console.log(competitorOne[0]+": "+competitorOne[2] + " " + competitorTwo[0]+":"+competitorTwo[2]);  // This console.logs the players health...Changed for array
-            console.log(competitors[0].health, competitors[1].health);
+            console.log(competitors[0].health, competitors[1].health);              // logs health of each player
             //check for victor
-            var results = winnerCheck();                                                                 // This variable is a function call that captures the result of the winnerCheck function
-            console.log(results);
+            var results = winnerCheck();                                            // This variable is a function call that captures the result of the winnerCheck function
+            console.log(results);                                                   // logs results
 
-            round_txt.innerHTML = 'Round #' + round + " Results:";                                                                       // This logs the result or information returned by the winnerCheck function
+            round_txt.innerHTML = 'Round #' + round + " Results:";                  // This logs the result or information returned by the winnerCheck function
             round++;
-            if (results==="no winner"){                                                                  // This if statement helps determine if there is a winner or if the rounds need to to continue
+            if (results==="no winner"){                                             // This if statement helps determine if there is a winner or if the rounds need to to continue
 
-                ernie_txt.innerHTML = competitors[0].name + ":  " + competitors[0].health;
-                bert_txt.innerHTML = competitors[1].name + ":  " + competitors[1].health; // This adds one to the round
-                //alert(competitorOne[0]+":"+competitorOne[2]+"  *ROUND "+round+" OVER"+"*  "+competitorTwo[0]+":"+competitorTwo[2]); //This alert post after every round that does not produce a winner// Changed for array
+                ernie_txt.innerHTML = competitors[0].name + ":  " + competitors[0].health;  //This prints Ernie's health is there is no winner
+                bert_txt.innerHTML = competitors[1].name + ":  " + competitors[1].health;   //This prints Berts health is there is no winner
 
-            } else{                                                                                     // Else will execute code if there is a winner or if everyone dies.
+            } else{                                                                 // Else will execute code if there is a winner or if Ernie and Bert share the cookies.
                 ernie_txt.innerHTML = results;
                 bert_txt.innerHTML = "";
 
-                button.removeEventListener("click", eatCookies, false);
+                button.removeEventListener("click", eatCookies, false);             // The button changes to Done and no longer runs the program
 
-                document.querySelector('.buttonblue').innerHTML = 'DONE!!!';
-                                                                                             // This puts an end to all this craziness. If a winner is determined before the ten rounds
-                                                                                                        // are over this ends the rounds
-                                                                                              // End of if else statement
-        }                                                                                               // End of for loop
-    }                                                                                                   // End of the fight function
+                document.querySelector('.buttonblue').innerHTML = 'DONE!!!';        // grabs the buttons text and sets it to DONE!
+
+        }                                                                                               // End of for if else
+    }                                                                                                   // End of the eatCookies function
 //______________________________Start of winnerCheck function___//
     function winnerCheck(){                                     // This function will measure the players health and determine if there is a winner
         console.log("in winnerCheck function");                 // This lets me know that my winnerCheck function is running
         var result="no winner";                                 // This variable is for the result. it is set to 'no winner'
-        if (competitors[0].health < 1 && competitors[1].health < 1)           // This if statement will give the proper result after determining the winner ..Changed for array
+        if (competitors[0].health < 1 && competitors[1].health < 1)           // This if statement will give the proper result after determining the winner
         {                                                       // The first condition is to determine if the results of both players dropped below 1 health points during the same round.
             result = "Everyone got cookies!";                   // This result is if both players health points drop below 1 during the same round. For my game the cookies are  shared ;)
         } else if(competitors[0].health < 1){                          // This else if condition determines if player one health drops below one first...Changed for array
